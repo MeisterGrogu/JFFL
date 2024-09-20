733670cff8e48dc8c9fae6c7dd0537b1c5cba1a2
Leopoldd <leopold.peters@aeg-nb.de>
2024-09-20 21:59:26 +0200

    adding scanner and test "compiler"

--- src/scanner.c
+++ src/scanner.c
@@ -17,3 +17,33 @@ void initScanner(const char* source){
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+static Token errorToken(const char* msg){
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = msg;
+    token.length = (int)strlen(msg);
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken(){
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    return errorToken("Unexpected character.");
+}
