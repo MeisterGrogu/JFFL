3318f4eb9c43bc8dba0958e409e8124d0774f38b
Leopoldd <leopold.peters@aeg-nb.de>
2024-09-20 17:05:47 +0200

    adding the basics of instruction execution

--- include/vm.h
+++ include/vm.h
@@ -5,9 +5,17 @@
 
 typedef struct {
     Chunk* chunk;
+    uint8_t* ip;
 } VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM(void);
 void freeVM(void);
+InterpretResult interpret(Chunk* chunk);
 
 #endif
