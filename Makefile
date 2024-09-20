81f1e1ef175b8e1cd18768c498944072de7e0ead
Leopoldd <leopold.peters@aeg-nb.de>
2024-09-20 15:43:46 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+JFFL.c:
+	gcc -Iinclude src/JFFL.c src/chunk.c src/memory.c src/debug.c src/value.c -o JFFL.exe
---
+++ include/common.h
@@ -0,0 +1,8 @@
+#ifndef jffl_common_h
+#define jffl_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
