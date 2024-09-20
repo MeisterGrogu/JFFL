8483436e37e2f5eb5380428c691b99d6ce8b8e1d
Leopoldd <leopold.peters@aeg-nb.de>
2024-09-20 19:44:14 +0200

    Adding support for binary the operators "+" "-" "/" "*"

--- src/vm.c
+++ src/vm.c
@@ -20,6 +20,12 @@ void freeVM(void){
 static InterpretResult run(){
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(op) \
+        do { \
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while (false)
 
         for (;;){
             #ifdef DEBUG_TRACE_EXECUTION
@@ -34,19 +40,27 @@ static InterpretResult run(){
             #endif
             uint8_t instruction;
             switch (instruction = READ_BYTE()) {
-            case OP_CONSTANT:
+            case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
-            case OP_RETURN:
+            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   push(-pop()); break;
+            case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
                 return INTERPRET_OK;
+            }
             }
         }
     
     #undef READ_CONSTANT
     #undef READ_BYTE
+    #undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk){
